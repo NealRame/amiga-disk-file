8e16731d50765b30eae1b0782dcb80b63c6a634e
Julien Graziano <julien.graziano@arobas-music.com>
2025-03-14 22:34:24 +0100

    refactor: replace try_info with try_from

--- packages/nr-adf-tools/src/ls.rs
+++ packages/nr-adf-tools/src/ls.rs
@@ -23,8 +23,7 @@ pub struct Args {
 pub fn run(args: &Args) -> Result<()> {
     let disk_data = fs::read(&args.amiga_disk_filepath)?;
     let disk = Disk::try_create_with_data(disk_data)?;
-
-    let fs: AmigaDos = Rc::new(RefCell::new(disk)).try_into()?;
+    let fs = AmigaDos::try_from(Rc::new(RefCell::new(disk)))?;
 
     for entry in fs.read_dir(&args.amiga_input_filepath)? {
         match entry {
