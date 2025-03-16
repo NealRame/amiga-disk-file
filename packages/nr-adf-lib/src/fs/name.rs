4e2017cea11163088c19f1a67f342e44c44be314
Julien Graziano <julien.graziano@arobas-music.com>
2025-03-16 21:22:45 +0100

    test: add test for `check_name` and `hash_name`

--- packages/nr-adf-lib/src/fs/name.rs
+++ packages/nr-adf-lib/src/fs/name.rs
@@ -6,14 +6,17 @@ use crate::errors::*;
 use super::InternationalMode;
 
 
+fn valid_name_char(c: u8) -> bool {
+    c >= b' ' && c != b':' && c != b'/' && c != 127
+}
+
 // Check if a (dir, file or volume) name is valid
 pub fn check_name(name: &str) -> Result<(), Error> {
-    for c in name.bytes() {
-        if c < b' ' || c == b':' || c == b'/' {
-            return Err(Error::InvalidNameError)
-        }
+    if name.bytes().all(valid_name_char) {
+        Ok(())
+    } else {
+        Err(Error::InvalidNameError)
     }
-    Ok(())
 }
 
 // adapted from https://github.com/lclevy/ADFlib/blob/master/src/adf_dir.c#L918
@@ -54,4 +57,28 @@ pub fn hash_name(
             hash & 0x07ff
         })
         .rem(BLOCK_SIZE/4 - 56)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_name_fails_with_an_invalid_name() {
+        assert_eq!(check_name("foo:bar"), Err(Error::InvalidNameError));
+        assert_eq!(check_name("foo/bar"), Err(Error::InvalidNameError));
+    }
+
+    #[test]
+    fn check_name_pass_with_a_valid_name() {
+        assert_eq!(check_name("foo"), Ok(()));
+        assert_eq!(check_name("amiga"), Ok(()));
+    }
+
+    #[test]
+    fn hash_name_is_ok() {
+        assert_eq!(hash_name("foo", InternationalMode::Off), 15);
+        assert_eq!(hash_name("bar", InternationalMode::Off), 24);
+    }
 }
